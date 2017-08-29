58ce1bf68069df1d01568c50f6334e2676dff6fd
powi442 <k08ip01@gmail.com>
2017-08-29 08:47:06 -0700

    added grammar but still has a callback issue

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,41 +16,82 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  start();
+  console.log("connected as id " + connection.threadId + "\n");
+  queryProduce();
+  updateProduct();
 
 });
 
-function start() {
-  connection.query("SELECT product_name, department_name FROM produce", function(err, res) {
+function queryProduce() {
+  connection.query("SELECT * FROM produce", function(err, res) {
+    console.log("\n");
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
+  });
+
+}
+
+function updateProduct() {
+  connection.query("SELECT * FROM produce", function(err, results) {
     if (err) throw err;
-    console.log(res);
-  // ask the user what they would like to buy, and how much they would like to buy
-    inquirer.prompt([
-      {
-        name: "buy",
-        type: "input",
-        message: "What would you like to buy?",
-      },
-      {
-        name: "quantity",
-        type: "input",
-        message: "How many would you like to buy? (number)",
-      }
-    ]).then(function(answer) {
-      var ? = answer.input
-      var sql = "UPDATE customers SET stock_quantity=? where stock_quantity = " ;
-        con.query(sql, function (err, result) {
-      if (err) throw err;
-        console.log(result.affectedRows + " record(s) updated");
-    });
-
-      [
-        {
-
+  inquirer.prompt([{
+      name: "buy",
+      type: "rawlist",
+      choices: function() {
+        var choiceArray = [];
+        for (var i = 0; i < results.length; i++){
+          choiceArray.push(results[i].item_id);
         }
-      ]
-    )
-    })
-    })
-  }
+        return choiceArray;
+      },
+      message: "What would you like to buy?",
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many would you like to buy?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }
+  ]).then(function(answer) {
+        console.log("Computing.....\n");
+        var chosenProduce;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_name === answer.quantity) {
+            chosenProduce = results[i];
+          }
+        }
+        if(chosenProduce.stock_quantity < parseInt(answer.quantity)){
+
+          connection.query(
+            "UPDATE produce SET ? WHERE ?",
+            [
+              {
+                item_id: answer.buy
+              },
+              {
+                stock_quantity: answer.quantity
+              }
+            ],
+            function(err) {
+              if (error) throw err;
+              console.log("Congratulations on your purchase!")
+              queryProduce();
+            }
+          );
+        }
+        else {
+          console.log("Sorry, we don't have enough inventory for you, rockstar. Try again");
+          queryProduce();
+        }
+
+
+  });
+});
+}
