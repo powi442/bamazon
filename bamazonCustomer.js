fb8f2ee2b8936f740e96b98ac5cb35f499c9419b
powi442 <k08ip01@gmail.com>
2017-08-29 09:56:37 -0700

    added readme and screenshot

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -34,32 +34,44 @@ function queryProduce() {
 }
 
 function updateProduct() {
+
   connection.query("SELECT * FROM produce", function(err, results) {
     if (err) throw err;
-  inquirer.prompt([{
-      name: "buy",
-      type: "rawlist",
-      choices: function() {
-        var choiceArray = [];
-        for (var i = 0; i < results.length; i++){
-          choiceArray.push(results[i].item_id);
-        }
+    console.log(results);
+    console.log("thisworks!");
+    var choices = function() {
+      var choiceArray = [];
+      for (var i = 0; i < results.length; i++) {
+        choiceArray.push(results[i].item_id);
         return choiceArray;
-      },
-      message: "What would you like to buy?",
-    },
-    {
-      name: "quantity",
-      type: "input",
-      message: "How many would you like to buy?",
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
       }
     }
-  ]).then(function(answer) {
+
+    inquirer.prompt([{
+      name: "buy",
+      type: "list",
+      choices: choices,
+      message: "What would you like to buy?",
+    }
+  ]).then(function(answer, err) {
+      console.log(err);
+      console.log("thisworks!");
+
+      inquirer.prompt([{
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to buy?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+
+      ]).then(function(answer) {
+        console.log("thisworks!");
+
         console.log("Computing.....\n");
         var chosenProduce;
         for (var i = 0; i < results.length; i++) {
@@ -67,12 +79,9 @@ function updateProduct() {
             chosenProduce = results[i];
           }
         }
-        if(chosenProduce.stock_quantity < parseInt(answer.quantity)){
+        if (chosenProduce.stock_quantity < parseInt(answer.quantity)) {
 
-          connection.query(
-            "UPDATE produce SET ? WHERE ?",
-            [
-              {
+          connection.query("UPDATE produce SET ? WHERE ?", [{
                 item_id: answer.buy
               },
               {
@@ -85,13 +94,13 @@ function updateProduct() {
               queryProduce();
             }
           );
-        }
-        else {
+        } else {
           console.log("Sorry, we don't have enough inventory for you, rockstar. Try again");
           queryProduce();
         }
 
 
-  });
-});
+      });
+    })
+  })
 }
---
+++ reame.md
@@ -0,0 +1,19 @@
+Welcome to Bamazon customer edition!
+
+This is a readme.md to instruct you about how this program works.
+
+There are three main objectives of this demonstration:
+  1. Setting up a database with prices and quantity
+  2. Accessing the database through Node
+  3. Making changes to the database
+
+  See picture #1 to see what the list of produce that we will be selling in the Bamazon. Each item is unique and carries prices.
+
+  Choose one of the ids and this will be used to target one of the products.
+
+  Then you will input an amount of the product you'd like to produce.
+
+
+This selection will compare the amount you want with the inventory(stock_quantity) in Bamazon. If you do not have enough in your inventory, then you will receive an error message and will have to try again.
+
+If you have enough inventory, then this will update the server and it will show the difference. 
